1290664df672d76de592d35e46bd858584a0d296
Kelvin <quocvm1@gmail.com>
2024-08-14 16:09:52 +0800

    add Foc fo 148, args check, files valid check

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::path::Path;
 use tokio::fs::OpenOptions;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::process::Command;
@@ -41,7 +42,7 @@ async fn run_flash_command(
     let mut output = String::new();
     let mut success = false;
 
-    while let Ok(line) = time::timeout(Duration::from_secs(60), reader.next_line()).await {
+    while let Ok(line) = time::timeout(Duration::from_secs(300), reader.next_line()).await {
         if let Some(line) = line.expect("Failed to read line") {
             println!("{}", line);
             output.push_str(&line);
@@ -84,9 +85,22 @@ async fn run_flash_command(
 async fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 5 {
+        eprintln!("Usage: {} <timeout_in_ms> <number_of_repeats> <118binary_directory> <148binary_directory>", args[0]);
+        std::process::exit(1);
+    }
+
     let timeout = &args[1];
     let num_repeats: usize = args[2].parse().expect("Invalid number of repeats");
-    let dir = &args[3];
+    let dir_118 = &args[3];
+    let dir_148 = &args[4];
+
+    if (!Path::new(dir_118).exists() || !Path::new(dir_118).is_file())
+        || (!Path::new(dir_148).exists() || !Path::new(dir_148).is_file())
+    {
+        eprintln!("Error: The provided binary directory path is not valid or does not exist.");
+        std::process::exit(1);
+    }
 
     let log_file = "error_log.txt";
     let mut file = OpenOptions::new()
@@ -101,15 +115,16 @@ async fn main() {
 
         time::sleep(Duration::from_secs(5)).await;
 
-        let res118 = run_flash_command("118", timeout, &mut file, dir, i + 1).await;
+        let res118 = run_flash_command("118", timeout, &mut file, dir_118, i + 1).await;
         if !res118 {
             println!("Flashing S32K118 failed on iteration {}", i + 1);
         }
+        time::sleep(Duration::from_secs(5)).await;
 
-        // let res148 = run_flash_command("148", timeout, &mut file, dir, i + 1).await;
-        // if !res148 {
-        //     println!("Flashing S32K148 failed on iteration {}", i + 1);
-        // }
+        let res148 = run_flash_command("148", timeout, &mut file, dir_148, i + 1).await;
+        if !res148 {
+            println!("Flashing S32K148 failed on iteration {}", i + 1);
+        }
     }
     println!("Completed {} iterations.", num_repeats);
 }
